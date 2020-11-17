ccba6deb3e518d32848d6a2627739be2b13e3ffb
LiteHell <litehell@litehell.info>
2020-11-17 23:21:15 +0900

    Return error if setq fails

--- C_LISP_Interpreter/symbol.c
+++ C_LISP_Interpreter/symbol.c
@@ -1,18 +1,15 @@
 #include "lisp_processor_functions_symbol.h"
 #include "lisp_processor.h"
+#include "lisp_processor_utils.h"
 
 obj_t fn_setq(obj_t* pObj)
 {
 	const obj_t* symbol = pObj->list.value;
 	const obj_t* value = pObj->list.next->list.value;
-	if (!symbol || !value) {
-		exit(1);
-		return;
-	}
-	else if (symbol->type != SYMBOL) {
-		exit(1);
-		return;
-	}
+	if (!symbol || !value)
+		return create_error();
+	else if (symbol->type != SYMBOL)
+		return create_error();
 	obj_t evaluated = evaluateObject(value);
 	dict_insert(symbolsDict, symbol->symbol.symbol, &evaluated);
 	return evaluated;
