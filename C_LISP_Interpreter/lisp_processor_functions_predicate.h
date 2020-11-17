4e8a5fe26ae5a19eb0af8ad0b22e86524cd26c66
LiteHell <litehell@litehell.info>
2020-11-17 23:28:45 +0900

    Fix typos in header and make control/predicate functions available

--- C_LISP_Interpreter/lisp_processor_functions_predicate.h
+++ C_LISP_Interpreter/lisp_processor_functions_predicate.h
@@ -1,5 +1,5 @@
-#ifndef _H_LISP_PROC_SYMBOL
-#define _H_LISP_PROC_SYMBOL
+#ifndef _H_LISP_PROC_PREDICATE
+#define _H_LISP_PROC_PREDICATE
 #include "lisp_processor.h"
 obj_t fn_atom(obj_t* pObj);
 obj_t fn_null(obj_t* pObj);
@@ -10,7 +10,7 @@ obj_t fn_equal(obj_t* pObj);
 obj_t fn_lessthan(obj_t* pObj);
 obj_t fn_greaterOrEqualThan(obj_t* pObj);
 obj_t fn_stringp(obj_t pObj);
-#define LISP_SYMBOL_FUNC_COUNT 9
-#define LISP_SYMBOL_FUNC_NAMES "ATOM", "NULL", "NUMBERP", "ZEROP", "MINUSP", "EQUAL", "<", ">=", "STRINGP"
-#define LISP_SYMBOL_FUNCS fn_atom, fn_null, fn_numberp, fn_zerop, fn_minusp, fn_equal, fn_lessthan, fn_greaterOrEqualThan, fn_stringp
+#define LISP_PREDICATE_FUNC_COUNT 9
+#define LISP_PREDICATE_FUNC_NAMES "ATOM", "NULL", "NUMBERP", "ZEROP", "MINUSP", "EQUAL", "<", ">=", "STRINGP"
+#define LISP_PREDICATE_FUNCS fn_atom, fn_null, fn_numberp, fn_zerop, fn_minusp, fn_equal, fn_lessthan, fn_greaterOrEqualThan, fn_stringp
 #endif
